9ed2346335a2236c48121aa938f600c55165d87a
kelibst <kbooster17@gmail.com>
2020-07-14 20:54:37 +0000

    added remove funcitonality

--- assets/scripts/book.js
+++ assets/scripts/book.js
@@ -26,7 +26,7 @@ var book_list = [
 
 
 var render = function(template, node, container = document.createElement("div") ){
-    container.setAttribute("class", "col-sm-4");
+    
     container.innerHTML = template;
     node.appendChild(container)
 }
@@ -36,11 +36,14 @@ var render = function(template, node, container = document.createElement("div") 
 book_list.forEach(function(book){
     
 var node = document.getElementById("title");
-    var template = `<div class="card" style="width: 18rem;">
+    var template = `<div class="card m-4 text-center">
                     <div class="card-body">
                     <h5 class="card-title">${book.name}</h5>
-                    <p class="card-text">${book.title}.</p>
-                    <a href="#" class="btn btn-primary">${book.pages}</a>
+                    <p class="card-text">${book.title}</p>
+                    
+                    <p class="card-text text-center"><span class="font-weight-bolder">Number of pages:  ${book.pages}</span></p>
+                
+                    <a href="#" class="btn btn-danger remove w-100">Remove Book</a>
                     </div>
                 </div>`;
     render(template,node)
@@ -53,24 +56,42 @@ button = document.querySelector("button")
 
 button.addEventListener('click', function(e){
     e.preventDefault();
-    if(form.book_title.value.length<3 || form.author_name.value.length < 3){
+    if(form.book_title.value.length < 3 || form.author_name.value.length < 3){
         errors.push("Sorry Book title and Author name should be at least 3 characters long!")
     }
     book_list.push(new Book(form.book_title.value, form.author_name.value, form.book_pages.value, form.book_status.value),);
-
-
-    
-
     
     var node = document.getElementById("title");
-    var template = `<div class="card" style="width: 18rem;">
+    var template = `<div class="card text-center m-4">
                     <div class="card-body">
                     <h5 class="card-title">${form.author_name.value}</h5>
-                    <p class="card-text">${form.book_title.value}.</p>
-                    <a href="#" class="btn btn-primary">${form.book_pages.value}</a>
+                    <p class="card-text">${form.book_title.value}</p>
+                    <p class="card-text text-center"><span class="font-weight-bolder">Number of pages:  ${form.book_pages.value}</span></p>
+                  
+                    <a href="#" class="btn btn-danger remove w-100">Remove Book</a>
                     </div>
                 </div>`;
     render(template,node)
     form.reset();
+})
+
+shelve = document.querySelector('.book-lists');
+
+shelve.addEventListener('click', function(e){
+    if (e.target.classList.contains("remove")){
+        var text = e.target.parentElement.children[1].innerText;
+       
+        book_list = book_list.filter(function(books){
+            if(books.title != text){
+        
+                return books;        
+            }
+            
+            
+        });
+    
+        console.log(book_list);
+     e.target.parentElement.remove();
+    }
 })
 

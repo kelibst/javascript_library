05cf824df39efd06d9587293c699331319eeb8d2
kelibst <kbooster17@gmail.com>
2020-07-14 16:45:34 +0000

    added form to add book

--- assets/scripts/book.js
+++ assets/scripts/book.js
@@ -23,10 +23,54 @@ var book_list = [
     new Book('The Call of The Wild', 'Jack London', '80'),
     new Book("Hitchiker's Guide to The Galaxy", 'Douglas Adams', '250', true),
 ]
-const  Harry = new Book("Harry poter", "MG", 346, false)
 
-book_list.forEach(item, function(){
-    console.log(item)
+
+var render = function(template, node, container = document.createElement("div") ){
+    container.setAttribute("class", "col-sm-4");
+    container.innerHTML = template;
+    node.appendChild(container)
+}
+
+
+
+book_list.forEach(function(book){
+    
+var node = document.getElementById("title");
+    var template = `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                    <h5 class="card-title">${book.name}</h5>
+                    <p class="card-text">${book.title}.</p>
+                    <a href="#" class="btn btn-primary">${book.pages}</a>
+                    </div>
+                </div>`;
+    render(template,node)
 });
 
-var title = document.getElementById("title");
+var errors = [];
+
+form = document.querySelector('form');
+button = document.querySelector("button")
+
+button.addEventListener('click', function(e){
+    e.preventDefault();
+    if(form.book_title.value.length<3 || form.author_name.value.length < 3){
+        errors.push("Sorry Book title and Author name should be at least 3 characters long!")
+    }
+    book_list.push(new Book(form.book_title.value, form.author_name.value, form.book_pages.value, form.book_status.value),);
+
+
+    
+
+    
+    var node = document.getElementById("title");
+    var template = `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                    <h5 class="card-title">${form.author_name.value}</h5>
+                    <p class="card-text">${form.book_title.value}.</p>
+                    <a href="#" class="btn btn-primary">${form.book_pages.value}</a>
+                    </div>
+                </div>`;
+    render(template,node)
+    form.reset();
+})
+
